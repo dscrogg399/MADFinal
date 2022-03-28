f1bb6e1932e182ef10c10176ed1b74d72fdb671c
dscrogg399 <81265118+dscrogg399@users.noreply.github.com>
2022-03-28 14:37:40 -0500

    testing the discord bot
    added a comment to see how the discord bot works

--- app/src/main/java/cs/mad/pantree/IngredientInputActivity.kt
+++ app/src/main/java/cs/mad/pantree/IngredientInputActivity.kt
@@ -7,6 +7,7 @@ class IngredientInputActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //fdsfsadfdsafsads
+        //testing the discord bot
         setContentView(R.layout.activity_ingredient_input)
     }
 }
